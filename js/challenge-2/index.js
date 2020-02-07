0833979e71ab43681ef7ffba9749aa8d4ac51b87
Jason Campos <jcampos8782@gmail.com>
2020-02-07 10:31:17 -0800

    Add second challenge

---
+++ js/challenge-2/index.js
@@ -0,0 +1,15 @@
+/*
+	Daily Challenge 2/7/2020
+	
+	Write a function which accepts a string as input and outputs an array which indicates 
+	how many times a character was encountered in that string. The position in the array
+	indicates the position in the alphabet of the letter. Ignore all spaces and special 
+	characters.
+
+	Input: A string
+	Output: An array of numbers indicating the number of times a character was encountered
+
+	Example:
+	input: "A string of characters"
+	output: [2, 0, 2, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0] 
+*/
