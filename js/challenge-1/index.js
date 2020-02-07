b5046c0ea83067523d31f97948bb62e33422f7fa
Jason Campos <jcampos8782@gmail.com>
2020-02-07 10:16:13 -0800

    Add first challenge

---
+++ js/challenge-1/index.js
@@ -0,0 +1,15 @@
+/*
+	Daily Challenge: 2/7/2020
+	
+	Write a function which accepts an array of N numbers as input and returns an array
+	of all of the products of N-1 numbers. 
+
+	Input: Array of N numbers
+	Output: Array of all products of N-1 numbers
+
+	Example: 
+
+	input: [1,2,3,6]
+	output: [1 * 2 * 3, 1 * 2 * 6, 1 * 3 * 6, 2 * 3 * 6] 
+*/
+
