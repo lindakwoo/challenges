949ba20b63dd14d4fd8c11bd70f88411461bd2b5
Jason Campos <jcampos8782@gmail.com>
2020-02-11 11:25:20 -0800

    Add challenge 3

---
+++ js/challenge-3/index.js
@@ -0,0 +1,16 @@
+/*
+	Daily Challenge 2/11/2020
+
+ Write a function which accepts a string representation of a number
+ and returns the longest prime present in the string or null if
+ no prime exists.
+
+ For example, "130" should return 13. Remember, the string can be
+ much larger than this so try to account for that in your implementation.
+
+ What is the runtime complexity (Big O notation) of your solution?
+
+	Input: A string
+	Output: The largest prime or null if no prime exists
+
+*/
