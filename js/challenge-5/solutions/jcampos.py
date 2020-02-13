4efbf807b498a5bb852057c96e99218ffbaea4ed
Jason Campos <jcampos8782@gmail.com>
2020-02-13 12:22:15 -0800

    Remove collections import. Implement simple doubly-linked-list to provide all O(1) operations

--- js/challenge-5/solutions/jcampos.py
+++ js/challenge-5/solutions/jcampos.py
@@ -1,19 +1,13 @@
-import collections
-
-
 class LruCache:
 
     # The cache will hold a key and a reference to an object in the linked list.
     # This will allow for O(1) lookups, removals, and updates in the queue
     __cache = None
 
-    # deque is a doubly-linked list internally and provides O(1) removal and appends
-    # This structure will hold the objects themselves.
-    __queue = None
-
     def __init__(self, capacity):
         self.__capacity = capacity
-        self.__queue = collections.deque()
+        self.__head = None
+        self.__tail = None
         self.__cache = {}
 
     def get(self, key):
@@ -22,8 +16,8 @@ class LruCache:
 
         # Pull the item from the cache and place it back at the end of the queue.
         item = self.__cache[key]
-        self.__queue.remove(item)
-        self.__queue.append(item)
+        self.__splice(item)
+        self.__append(item)
         return item[1]
 
     def put(self, key, value):
@@ -31,15 +25,53 @@ class LruCache:
         # Since we aren't changing the size of the queue, we don't need to perform
         # a capacity check
         if key in self.__cache:
-            self.__queue.remove(self.__cache[key])
+            item = self.__cache[key]
+            self.__splice(item)
         # If the cache is at capacity, remove the oldest entry from the cache
         elif len(self.__cache) == self.__capacity:
-            del self.__cache[self.__queue.popleft()[0]]
+            self.__pop()
 
-        # Create a tuple that will be referenced in the list and by the cache.
-        item = (key, value)
+        item = [key, value, None, self.__head]
+        self.__append(item)
         self.__cache[key] = item
-        self.__queue.append(item)
+
+    def __splice(self, item):
+        # Splice this item out of the list
+        prv = item[2]
+        nxt = item[3]
+
+        # If this is the last item in the list, set the prv as the new tail
+        if nxt is None:
+            self.__tail = prv
+        else:
+            nxt[2] = prv
+
+        # If this is the first item in the list, set the head
+        if prv is None:
+            self.__head = nxt
+        else:
+            prv[3] = nxt
+
+    def __is_empty(self):
+        return len(self.__cache) == 0
+
+    def __append(self, item):
+        if not self.__is_empty():
+            self.__head[2] = item
+            item[3] = self.__head
+
+        item[2] = None
+        self.__head = item
+
+        if self.__tail is None:
+            self.__tail = item
+
+    def __pop(self):
+        if not self.__is_empty():
+            new_tail = self.__tail[2]
+            new_tail[3] = None
+            del self.__cache[self.__tail[0]]
+            self.__tail = new_tail
 
 
 cache = LruCache(3)
